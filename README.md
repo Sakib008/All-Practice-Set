92814e27a1ee2ac4acfe170a69c1843d69a1f2f8
Sakib008 <mdshakib1902@gmail.com>
2024-11-10 21:34:08 +0530

    feat : add react useReducer Practice

---
+++ README.md
@@ -0,0 +1,11 @@
+# React Practice Sets and Projects for the Practice 
+
+
+## Set 1 : - useReducer Practice 
+
+Practice some question of useReducer to sharpen the useReducer's Knowledge
+
+
+## Screenshots
+
+![Set 1 of React](c:\Users\pc\Pictures\Screenshots\react1.png) 
---
+++ practice-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Reducer1 } from './useReducer/reducer1';
+
+function App() {
+  return (
+    <div className="App max-w-[1700px] mx-auto text-green-300">
+      <h1 className='text-6xl text-green-300 text-center font-semibold underline underline-offset-8 decoration-2 '>This is a Practice Set 1</h1>
+      <Reducer1/>
+    </div>
+  );
+}
+
+export default App;
