564d550090d90b6bdef9ceae4f5fd00e679a7b07
Sakib008 <mdshakib1902@gmail.com>
2024-11-13 22:18:16 +0530

    feat : Router and Context add

--- practice-react/src/index.js
+++ practice-react/src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { TodoContextProvider } from "./useReducer/Contexts/todoContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <TodoContextProvider>
+        <App />
+      </TodoContextProvider>
+    </Router>
   </React.StrictMode>
 );
