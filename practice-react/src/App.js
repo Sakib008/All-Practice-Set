a4353de7428f1a1037c8eaf1d0376c2061501487
Sakib008 <mdshakib1902@gmail.com>
2024-11-13 22:17:45 +0530

    feat : router and Navlink

--- practice-react/src/App.js
+++ practice-react/src/App.js
@@ -1,11 +1,23 @@
+import {NavLink,Routes,Route} from 'react-router-dom';
+
 import './App.css';
-import { Reducer1 } from './useReducer/reducer1';
+import {Todo} from './useReducer/pages/Todo';
+// import { Reducer1 } from ' ./useReducer/reducer1';
 
 function App() {
   return (
     <div className="App max-w-[1700px] mx-auto text-green-300">
-      <h1 className='text-6xl text-green-300 text-center font-semibold underline underline-offset-8 decoration-2 '>This is a Practice Set 1</h1>
-      <Reducer1/>
+     {/* <Reducer1/> */}
+     <div className="flex flex-col">
+      <div class="w-full h-1 bg-blue-500 my-2"></div>
+      <NavLink to="/todo" className="text-2xl p-2 rounded-lg my-2 mx-auto text-white bg-indigo-800 ">
+        Todo List
+      </NavLink>
+      <Routes>
+        <Route path='/todo' element={<Todo />} />
+      </Routes>
+      
+    </div>
     </div>
   );
 }
