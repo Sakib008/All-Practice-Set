38bc3f2acc59a9cb6a097d4fd91284841b284bc0
Sakib008 <mdshakib1902@gmail.com>
2024-11-13 22:19:34 +0530

    feat : Main Page of the Todo

---
+++ practice-react/src/useReducer/pages/Todo.js
@@ -0,0 +1,13 @@
+import { TodoHeader } from "../components/todo/todoHeader";
+
+export function Todo() {
+  return (
+    <div class="">
+      <h1 className="text-5xl text-green-300 mt-2 text-center font-semibold underline underline-offset-8 decoration-2 ">
+        This is Set 2 &#10088; To-Do &#10089;{" "}
+      </h1>
+      <TodoHeader />
+
+    </div>
+  );
+}
