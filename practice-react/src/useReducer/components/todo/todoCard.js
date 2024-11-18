2595579ba5ea2c25836cf72cb70d1565563b802c
Sakib008 <mdshakib1902@gmail.com>
2024-11-18 07:27:58 +0530

    fix : change according to single todo

--- practice-react/src/useReducer/components/todo/todoCard.js
+++ practice-react/src/useReducer/components/todo/todoCard.js
@@ -1,14 +1,24 @@
-export function TodoCard({ todo,isDone }) {
-  const { id, title, description, isCompleted } = todo;
+import {Link} from 'react-router-dom';
+import {useContext} from 'react';
+
+import {TodoContext} from '../../Contexts/todoContext';
+export function TodoCard(todo ) {
+  const { id, title, description, isCompleted,isDone,isSingle } = todo;
+  const {handleSingleTodo} = useContext(TodoContext);
+
+  
   return (
-    <div className="bg-purple-600 w-4/5 rounded-lg m-2 p-2 mx-auto text-white" key={id}>
+    <div className="bg-purple-600 w-4/5 rounded-lg m-2 p-2 mx-auto text-white" >
       <h1 className="text-xl font-bold">{title}</h1>
       <p className="">{description}</p>
       <p className=" ">Status : {isCompleted ? 'Done' : 'Not Done'}</p>
       {isDone &&
       (
-        <button class="bg-rose-400 p-1 rounded-md my-2">Expend Todo</button>
+        <Link className="bg-rose-400 p-1 rounded-md my-2" to={`/todo/${id}`} onClick={()=>handleSingleTodo(todo)} >Expend Todo</Link>
       )}
+      {isSingle && 
+      <Link className="bg-rose-400 p-1 rounded-md my-2" to={`/todo/done`} >Back</Link>
+      }
     </div>
   );
 }
