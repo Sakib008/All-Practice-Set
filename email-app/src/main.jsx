b5d1c1e19268f77008c96b0dbaebce4ab6065949
Sakib008 <mdshakib1902@gmail.com>
2024-11-25 22:30:14 +0530

    fix : update useContext api

--- email-app/src/main.jsx
+++ email-app/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+import App from "./App.jsx";
+import { EmailContextProvider } from "./Context/EmailContext.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <EmailContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </EmailContextProvider>
+  </StrictMode>
+);
