b1802ac1aef14cace731907b4f6f5c6283a19130
Sakib008 <mdshakib1902@gmail.com>
2024-11-26 21:46:29 +0530

    fix : updated Mark as Read Function

--- email-app/src/Context/EmailContext.jsx
+++ email-app/src/Context/EmailContext.jsx
@@ -4,9 +4,9 @@ import { fakeFetch } from "../Api/EmailApi";
 
 export const emailContext = createContext();
 
-export const EmailContextProvider = ({children}) => {
+export const EmailContextProvider = ({ children }) => {
   const [emailData, setEmailData] = useState([]);
-//   const [sendEmail, setSendEmail] = useState([]);
+  //   const [sendEmail, setSendEmail] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   const handleEmail = async () => {
@@ -14,7 +14,7 @@ export const EmailContextProvider = ({children}) => {
     try {
       const res = await fakeFetch("https://example.com/api/allemails");
       setEmailData(res.data);
-    //   setSendEmail(res.data.sentEmails)
+      //   setSendEmail(res.data.sentEmails)
       setIsLoading(false);
     } catch (err) {
       console.error(err);
@@ -22,10 +22,20 @@ export const EmailContextProvider = ({children}) => {
     }
   };
 
+  const markAsRead = (id) => {
+    setEmailData((prevEmail) => ({
+      ...prevEmail,
+      emails: prevEmail.emails.map((email) =>
+        email.id === id ? { ...email, read: true } : email
+      ),
+    }));
+  };
 
-  return(
-    <emailContext.Provider value={{emailData,handleEmail,isLoading}} >
-        {children}
+  return (
+    <emailContext.Provider
+      value={{ emailData, handleEmail, isLoading, markAsRead }}
+    >
+      {children}
     </emailContext.Provider>
-  )
+  );
 };
