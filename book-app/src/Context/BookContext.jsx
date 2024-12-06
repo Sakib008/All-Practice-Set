5c6ade86d263418bcc3fc1a4156f3a74b0537472
Sakib008 <mdshakib1902@gmail.com>
2024-12-06 22:41:37 +0530

    fix : updated Reduce and added fetch function

--- book-app/src/Context/BookContext.jsx
+++ book-app/src/Context/BookContext.jsx
@@ -1,6 +1,9 @@
-import { createContext,useReducer } from "react";
+import { createContext, useReducer,useState } from "react";
 
-const BookContext = createContext();
+import {fakeFetch} from '../Api/BookApi';
+
+export const BookContext = createContext();
+
 
 export const BookContextProvider = ({ children }) => {
   const initialVal = { readBooks: [], favoriteBooks: [] };
@@ -18,28 +21,52 @@ export const BookContextProvider = ({ children }) => {
         if (!state.readBooks.find((book) => book.id === action.payload.id))
           return { ...state, readBooks: [...state.readBooks, action.payload] };
         return state;
-      case "REMOVE_FROM_READ":
-        if (state.favoriteBooks.filter((book) => book.id !== action.payload.id))
-          return {
-            ...state,
-            favoriteBooks: [...state.favoriteBooks, action.payload],
-          };
-        return state;
-
       case "REMOVE_FROM_FAVORITE":
-        if (state.readBooks.filter((book) => book.id !== action.payload.id))
-          return { ...state, readBooks: [...state.readBooks, action.payload] };
-        return state;
+        return {
+          ...state,
+          favoriteBooks: state.favoriteBooks.filter(
+            (book) => book.id !== action.payload.id
+          ),
+        };
+
+      case "REMOVE_FROM_READ":
+        return {
+          ...state,
+          readBooks: state.readBooks.filter(
+            (book) => book.id !== action.payload.id
+          ),
+        };
 
       default:
         return state;
     }
   };
 
-  const [state,dispatch] =useReducer(bookReducer,initialVal);
+  const [state, dispatch] = useReducer(bookReducer, initialVal);
+
+
+  // Fetching Data 
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [books, setBooks] = useState([]);
+  const [profile,setProfile] = useState({});
+
+
+  const handleBooks = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fakeFetch("https://latestbooks.com/api/books");
+      setBooks(response.data.books);
+      setProfile(response.data.user)
+      setIsLoading(false);
+    } catch (err) {
+      console.error(err);
+      setIsLoading(false);
+    }
+  };
 
   return (
-    <BookContext.Provider value={{ state,dispatch }}>
+    <BookContext.Provider value={{ state, dispatch,profile,isLoading,books,handleBooks }}>
       {children}
     </BookContext.Provider>
   );
